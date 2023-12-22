dcbfca1e8352af0594b1fea29657d5bd34397ad3
Radityo <radityohernanda@gmail.com>
2023-12-22 22:39:54 +0700

    start axum http server + health check + run watch + rest client example

--- readme.md
+++ readme.md
@@ -1,4 +1,30 @@
+Current Development Stack
+
+- rustc 1.73.0
+- cargo 1.73.0
+
 ```sh
 # Init Project
 cargo init raditzlawliet_rust-note-rest
+
+# Depedency
+cargo add axum
+cargo add tokio -F full
+cargo add tower-http -F "cors"
+cargo add serde_json
+cargo add serde -F derive
+cargo add chrono -F serde
+cargo add dotenv
+cargo add uuid -F "serde v4"
+cargo add sqlx --features "runtime-async-std-native-tls mysql chrono uuid"
+
+# Build & Run Project
+cargo build
+cargo run
+
+# For Automatically rebuild the project
+cargo install cargo-watch
+
+# Run with watch
+cargo watch -q -c -w src/ -x run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/healthcheck", get(health_check_handler));
+
+    println!("Server started successfully at 0.0.0.0:8080");
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+pub async fn health_check_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "API Services";
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
 }
