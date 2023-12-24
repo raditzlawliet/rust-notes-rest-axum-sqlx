0c9b6bfce30bbc9f2e058759582a68ad206bf3c4
Radityo <radityohernanda@gmail.com>
2023-12-24 16:50:42 +0700

    add api request schema

---
+++ src/schema.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+// List
+#[derive(Deserialize, Debug, Default)]
+pub struct FilterOptions {
+    pub page: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+// Read/Delete
+#[derive(Deserialize, Debug)]
+pub struct ParamOptions {
+    pub id: String,
+}
+
+// Create
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateNoteSchema {
+    pub title: String,
+    pub content: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub is_published: Option<bool>,
+}
+
+// Update
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateNoteSchema {
+    pub title: Option<String>,
+    pub content: Option<String>,
+    pub is_published: Option<bool>,
+}
