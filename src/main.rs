019a6de780a164923f0aa04d2e4a1703ec577ab5
Radityo <radityohernanda@gmail.com>
2023-12-24 14:44:20 +0700

    connect to mysql server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
+use std::sync::Arc;
+
 use axum::{response::IntoResponse, routing::get, Json, Router};
+
+use dotenv::dotenv;
 use tokio::net::TcpListener;
+
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+pub struct AppState {
+    db: MySqlPool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/healthcheck", get(health_check_handler));
+    dotenv().ok();
+    println!("🌟 REST API Service 🌟");
 
-    println!("Server started successfully at 0.0.0.0:8080");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must set");
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅ Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("❌ Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app = Router::new()
+        .route("/api/healthcheck", get(health_check_handler))
+        .with_state(Arc::new(AppState { db: pool.clone() }));
+
+    println!("✅ Server started successfully at 0.0.0.0:8080");
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app.into_make_service())
